52301a83728b054f1c4995726863c472744afb61
hu-ng <hung.nguyen.2@minerva.kgi.edu>
2020-04-07 23:52:06 -0300

    fixed regex

--- code/script.py
+++ code/script.py
@@ -9,15 +9,17 @@ class ClassMaterialEventHandler(RegexMatchingEventHandler):
         super(ClassMaterialEventHandler, self).__init__(*args, **kwargs)
         self.last_created = None
         self.last_modified = None
+
+        # Change this to choose another target directory
         self.target_dir = "C:\\Users\Hung Nguyen\Desktop\Readings and works\\3rd Year\Second Sem"
-    
+
 
     def get_class_path(self, path):
         """
         Find the folder for the right class and session.
         If it doesn't exist, create a new one and return the absolute path.
         """
-        pattern = r"(?P<name>\w\w?[1-9]{3})_(?P<week>1?[1-9])_(?P<session>[1-2])_?.*"
+        pattern = r"(?P<name>\w\w?[1-9]{3})_(?P<week>[1-9]|1[0-5])_(?P<session>[1-2])_?.*"
         class_detail = re.match(pattern, path.split("\\")[-1])
         class_name = class_detail.group("name").upper()
         class_week = class_detail.group("week")
@@ -51,7 +53,7 @@ class ClassMaterialEventHandler(RegexMatchingEventHandler):
             self.last_modified = path
             to_path = self.get_class_path(path)
             shutil.move(path, to_path)
-            print(f"Move to {to_path}")
+            print(f"Moved to {to_path}")
 
 
     def on_deleted(self, event):
@@ -66,10 +68,10 @@ class ClassMaterialEventHandler(RegexMatchingEventHandler):
 if __name__ == "__main__":
     # Assumes the file is always named: classname_week_session_abc.*
     # For example: "B111_1_2_marketing_reading.pdf"
-    event_handler = ClassMaterialEventHandler(regexes=['.*\w\w?[1-9]{3}_1?[1-9]_[1-2]_?.*'])
-    path = "C:\\Users\Hung Nguyen\Downloads"
+    event_handler = ClassMaterialEventHandler(regexes=['.*\w\w?[1-9]{3}_([1-9]|1[0-5])_[1-2]_?.*'])
+    watch_path = "C:\\Users\Hung Nguyen\Downloads"   # Change this to watch another directory
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=False)
+    observer.schedule(event_handler, watch_path, recursive=False)
     observer.start()
 
     try:
